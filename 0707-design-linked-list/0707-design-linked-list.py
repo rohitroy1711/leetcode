78ea21e60fd5630ce64aa93c04bfe9c15a0a1420
Rohit Roy <63733152+rohitroy1711@users.noreply.github.com>
2024-06-10 20:39:17 -0700

    Time: 99 ms (82.73%), Space: 17.9 MB (23.22%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.py
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -1,69 +1,69 @@
-class ListNode:
+class Listnode:
     def __init__(self,val):
         self.val = val
-        self.prev = None
+        self.prev=None
         self.next = None
+        
 class MyLinkedList:
 
     def __init__(self):
-        self.left = ListNode(0)
-        self.right = ListNode(0)
+        self.left = Listnode(0)
+        self.right = Listnode(0)
         self.left.next = self.right
         self.right.prev = self.left
-        
 
     def get(self, index: int) -> int:
-        cur = self.left.next
-        i = 0
-        while cur and i<index:
-            cur = cur.next
-            i = i+1
-        if i== index and cur and cur!=self.right:
-            return cur.val  
-        return -1
+        curr = self.left.next
+        while curr and index>0:
+            curr = curr.next
+            index-=1
+        if curr and curr!= self.right and index==0:
+            return curr.val
+        else:
+            return -1
+        
 
     def addAtHead(self, val: int) -> None:
-        node,next,prev = ListNode(val),self.left.next,self.left
-        next.prev = node
-        prev.next = node
-        node.prev = prev
-        node.next = next
+        temp = self.left.next
+        curr = Listnode(val)
+        self.left.next = curr
+        curr.prev = self.left
+        curr.next = temp
+        temp.prev = curr
         
 
     def addAtTail(self, val: int) -> None:
-        node,next,prev = ListNode(val),self.right,self.right.prev
-        next.prev = node
-        prev.next = node
-        node.prev = prev
-        node.next = next
+        temp = self.right.prev
+        curr = Listnode(val)
+        self.right.prev = curr
+        curr.next = self.right
+        temp.next = curr
+        curr.prev = temp
         
 
     def addAtIndex(self, index: int, val: int) -> None:
-        cur = self.left.next
-        while cur and index>0:
-            cur = cur.next
-            index -=1
-        if cur and index == 0:
-            node,next,prev = ListNode(val),cur,cur.prev
-            next.prev = node
-            prev.next = node
-            node.prev = prev
-            node.next = next
-            
+        curr = self.left.next
+        while curr and index>0:
+            curr = curr.next
+            index-=1
+        if curr and index==0:
+            node = Listnode(val)
+            temp = curr.prev
+            temp.next = node
+            node.prev = temp
+            node.next = curr
+            curr.prev = node
         
 
     def deleteAtIndex(self, index: int) -> None:
-        cur = self.left.next
-        while cur and index>0:
-            cur = cur.next
-            index -=1
-        if cur and index == 0 and cur!=self.right:
-            next,prev = cur.next,cur.prev
+        curr = self.left.next
+        while curr and index>0:
+            curr=curr.next
+            index-=1
+        if index ==0  and curr and curr!=self.right:
+            next,prev = curr.next,curr.prev
             next.prev = prev
             prev.next = next
-            
-            
-            
             
 
 
