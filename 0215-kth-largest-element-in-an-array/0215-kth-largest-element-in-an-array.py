d42c6a34e6ed0553efcb0421e68f2a9ff5ef467e
Rohit Roy <63733152+rohitroy1711@users.noreply.github.com>
2024-07-02 11:21:49 -0700

    Time: 507 ms (17.81%), Space: 28.8 MB (91.94%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,41 +1,13 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        s = 0
-        e = len(nums)
-        n = self.mergesortHelper(nums,s,e)
-        return n[e-k]
-    def mergesortHelper(self,arr:List[int],s,e)->List[int]:
-        if e-s+1 <= 1:
-            return arr
-        m = s+(e-s)//2
-        self.mergesortHelper(arr,s,m)
-        self.mergesortHelper(arr,m+1,e)
+        print(nums)
+        i = len(nums)-k
         
-        self.merge(arr,s,m,e)
-        return arr
+        heapq.heapify(nums)
         
-    def merge(self,arr:List[int],s,m,e)->None:
-        l = arr[s:m+1]
-        r = arr[m+1:e+1]
+        while i>0:
+            heapq.heappop(nums)
+            i=i-1
+        return nums[0]        
         
-        i = 0
-        j = 0
-        k = s
-        
-        while i<len(l) and j<len(r):
-            if l[i]<=r[j]:
-                arr[k] = l[i]
-                i+=1
-            else:
-                arr[k] = r[j]
-                j+=1
-            k+=1
-        while i<len(l):
-            arr[k] = l[i]
-            i+=1
-            k+=1
-        while  j<len(r):
-            arr[k] = r[j]
-            j+=1
-            k+=1
         
