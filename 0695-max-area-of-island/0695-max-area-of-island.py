bb157ab46b4d72589e44c3de8991ecf3835e2455
Rohit Roy <63733152+rohitroy1711@users.noreply.github.com>
2024-07-10 20:56:31 -0700

    Time: 126 ms (17.56%), Space: 17.1 MB (64.31%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,34 +2,38 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         if not grid:
             return 0
-        
-        rows,cols = len(grid),len(grid[0])
-        visited = set()
-        areas = []
-        islands =0
-        
-        def bfs(r,c):
-            q = collections.deque()
-            q.append((r,c))
-            visited.add((r,c))
-            directions = [[1,0],[0,1],[-1,0],[0,-1]]
+        rows = len(grid)
+        cols = len(grid[0])
+        visit = set()
+        #all_areas = []
+        def bfs(i,j) -> int:
+            q = collections.deque() 
+            q.append((i,j))
+            visit.add((i,j))
             temp = 1
+            directions = [[1,0],[0,1],[-1,0],[0,-1]]
+            
             while q:
                 row,col = q.popleft()
                 for dr,dc in directions:
                     r = row+dr
                     c = col+dc
-                    if r in range(rows) and c in range(cols) and grid[r][c] == 1 and (r,c) not in visited:
-                        temp = temp+1
+                    if r in range(rows) and c in range(cols) and grid[r][c] == 1 and (r,c) not in visit:
+                        temp +=1
                         q.append((r,c))
-                        visited.add((r,c))
-            areas.append(temp)
+                        visit.add((r,c))
+            #all_areas.append(temp)
+            return temp
+                    
+                
+            
         
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1 and (r,c) not in visited:
-                    bfs(r,c)
-                    islands += 1
-        if not areas:
-            return 0
-        return max(areas)
+        area = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1 and (i,j) not in visit:
+                    area = max(area,bfs(i,j))
+                    
+        
+        #print(all_areas)
+        return area
