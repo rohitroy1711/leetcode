8bf6bbe1ebf69c2e40a0f1c237180321efd89285
Rohit Roy <63733152+rohitroy1711@users.noreply.github.com>
2024-07-15 20:23:34 -0700

    Time: 448 ms (53.43%), Space: 18.1 MB (26.59%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,30 +1,23 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0] == 1:
-            return -1
-        rows = len(grid)
-        cols = len(grid[0])
-        visited = set()
-        q = collections.deque()
-        q.append((0,0))
-        visited.add((0,0))
-        length = 0
-        
-        direct = [[1,0],[0,1],[0,-1],[-1,0],[1,1],[-1,-1],[-1,1],[1,-1]]
+        rows,cols = len(grid),len(grid[0])
+        q = deque()
+        visit = set()
+        q.append((0,0,1))
+        visit.add((0,0))
+        directions = [[0,1],[1,0],[0,-1],[-1,0]
+                     ,[1,1],[1,-1],[-1,-1],[-1,1]]
+        leng = 1
         while q:
-            for i in range(len(q)):
-                row,col = q.popleft()
-                if row == rows-1 and col == cols-1:
-                    return length+1
-                for dr,dc in direct:
-                    nr = row+dr
-                    nc = col+dc
-                    if nr not in range(rows) or nc not in range(cols) or grid[nr][nc] == 1 or (nr,nc) in visited:
-                        continue
-                    q.append((nr,nc))
-                    visited.add((nr,nc))
-            length+=1
+            r,c,leng = q.popleft()
+            if r not in range(rows) or c not in range(cols) or grid[r][c] == 1:
+                continue
+            if r == rows-1 and c == cols-1:
+                return leng
+            for dr,dc in directions:
+                row = r+dr
+                col = c+dc
+                if (row,col) not in visit:
+                    q.append((row,col,leng+1))
+                    visit.add((row,col))
         return -1
-                
-        
-        
