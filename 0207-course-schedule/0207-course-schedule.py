a5dcacec45540beccc5c17f57c51c3c3eef78b25
Rohit Roy <63733152+rohitroy1711@users.noreply.github.com>
2024-07-18 21:22:08 -0700

    Time: 90 ms (55.74%), Space: 18.5 MB (23.78%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,22 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        premap = {i:[] for i in range(numCourses)}
+        preMap = {i:[] for i in range(numCourses)}
         for crs,pre in prerequisites:
-            premap[crs].append(pre)
-        visit = set()
+            preMap[crs].append(pre)
         
+        visit = set()
         def dfs(crs):
-            if premap[crs] == []:
-                return True
             if crs in visit:
                 return False
+            if preMap[crs] == []:
+                return True
+            
             visit.add(crs)
-            for pre in premap[crs]:
+            for pre in preMap[crs]:
                 if not dfs(pre): return False
             visit.remove(crs)
-            premap[crs] = []    
+            preMap[crs] = []
             return True
         
-        for crs in range(numCourses):
-            if not dfs(crs): return False
+        for i in range(numCourses):
+            if not dfs(i) : return False
         return True
