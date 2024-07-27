a56a66be33b660496cb2588b981f6ecc0ccfc0ce
Rohit Roy <63733152+rohitroy1711@users.noreply.github.com>
2024-07-27 11:44:03 -0700

    Time: 37 ms (39.24%), Space: 16.5 MB (13.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,10 @@
 class Solution:
-    cache = {}
-    count = 0
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        if n in self.cache:
-            return self.cache[n]
-        self.cache[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
-        return self.cache[n]
+        
+        one,two = 1,1
+        for i in range(n-1):
+            temp = one
+            one = one +two
+            two = temp
+        return one
+        
