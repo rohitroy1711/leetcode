e3a1137b5c46969487d15994fc520f84e02c9a5c
Rohit Roy <63733152+rohitroy1711@users.noreply.github.com>
2024-09-03 01:34:19 -0700

    Time: 38 ms (47.47%), Space: 16.5 MB (80.03%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,11 +1,14 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        ans = [[1]]
+        lis = []
+        if numRows == 0 :
+            return lis
+        lis.append([1])
         for i in range(2,numRows+1):
-            temp = [0]+ans[-1]+[0]
+            temp = [0]+lis[-1]+[0]
             row = []
             for j in range(i):
                 row.append(temp[j]+temp[j+1])
-            ans.append(row)
-        return ans
-        
+            lis.append(row)
+        return lis
+            
