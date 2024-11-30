204a6962b8a8c8c341439473c1b8d7709282aeb5
Rohit Roy <63733152+rohitroy1711@users.noreply.github.com>
2024-11-29 19:25:41 -0800

    Time: 0 ms (100.00%), Space: 12.1 MB (6.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,16 +1,30 @@
 class Solution(object):
     def uniquePaths(self, m, n):
-        memo = {}
-        def paths(i,j):
-            if i == m-1 and j == n-1:
-                return 1
-            if i > m-1 or j > n-1:
-                return 0
-            if (i,j) in memo:
-                return memo[(i,j)]
-            else:
-                memo[(i,j)] =  paths(i+1,j)+paths(i,j+1)
-                return memo[(i,j)]
-        return paths(0,0)
+        N = m+n-2
+        R = m-1
+        num = 1
+        den = 1
+        for i in range(0,R):
+            num = (N-i)*num
+            den = (R-i)*den
+        ans = num//den
+        return ans
+        
+#         We can also solve this problem using Permutations and combinations
+        
+        
+#         The below is the code using dynamic progrramming
+#         memo = {}
+#         def paths(i,j):
+#             if i == m-1 and j == n-1:
+#                 return 1
+#             if i > m-1 or j > n-1:
+#                 return 0
+#             if (i,j) in memo:
+#                 return memo[(i,j)]
+#             else:
+#                 memo[(i,j)] =  paths(i+1,j)+paths(i,j+1)
+#                 return memo[(i,j)]
+#         return paths(0,0)
         
         
